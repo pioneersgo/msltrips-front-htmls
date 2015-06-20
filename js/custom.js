3aae22f6b2fb35ac4db2dba38ef7c755b7ad0917
Saidur <saidurdev@gmail.com>
2015-06-21 00:11:25 +0600

    MSL - Imporve JS functionality - 21/06

--- js/custom.js
+++ js/custom.js
@@ -37,5 +37,70 @@ $(document).ready(function(e) {
 			$(".contact-drawer-tab").removeClass("drawer-open-desktop");
 		}
 	});
+	
+	
+	/* --------------------------------------------------------------------- */
+	/* 3. Back to top
+	/* --------------------------------------------------------------------- */
+
+	var offset = 300,
+		//browser window scroll (in pixels) after which the "back to top" link opacity is reduced
+		offset_opacity = 1200,
+		//duration of the top scrolling animation (in ms)
+		scroll_top_duration = 700,
+		//grab the "back to top" link
+		$back_to_top = $('.cd-top');
+
+	//hide or show the "back to top" link
+	$(window).scroll(function(){
+		( $(this).scrollTop() > offset ) ? $back_to_top.addClass('cd-is-visible') : $back_to_top.removeClass('cd-is-visible cd-fade-out');
+		if( $(this).scrollTop() > offset_opacity ) { 
+			$back_to_top.addClass('cd-fade-out');
+		}
+	});
+
+	//smooth scroll to top
+	$back_to_top.on('click', function(event){
+		event.preventDefault();
+		$('body,html').animate({
+			scrollTop: 0 ,
+		 	}, scroll_top_duration
+		);
+	});
+	
+	
+	/* --------------------------------------------------------------------- */
+	/* 4. Stricy Menu
+	/* --------------------------------------------------------------------- */
+	
+	var scrolled = false;
+	
+	$(window).scroll(function() {
+		if ($(this).scrollTop() > 1){  
+			$('header').addClass("sticky");
+		}else{
+			$('header').removeClass("sticky");
+		}
+	});
+	
+	
+	$(window).scroll( function(){  
+		if (96 < $(window).scrollTop() && !scrolled) {
+			$('header').animate({ top: '0px' });
+			scrolled = true;
+		}
+		if (96 > $(window).scrollTop() && scrolled) {
+			$('header').animate({top: '-100px'});
+			scrolled = false;     
+		}
+	});
+	
+	/* --------------------------------------------------------------------- */
+	/* 5. Nice Scroll 
+	/* --------------------------------------------------------------------- */
+	
+	$("html").niceScroll();
+	
+	
  
 });
